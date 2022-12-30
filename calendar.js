dc1967ed99b22ef81160e92217048be2ac7647b1
bee-codes <brigemmanuel@gmail.com>
2022-12-30 16:35:44 +0100

    Updated calendar

--- calendar.js
+++ calendar.js
@@ -17,6 +17,12 @@ var monthArr = [
     "NOVEMBER",
     "DECEMBER"
 ];
+var month30Arr = [
+    "APRIL",
+    "JUNE",
+    "SEPTEMBER",
+    "NOVEMBER",
+];
 var dayArr = [
     "SUNDAY",
     "MONDAY",
@@ -26,12 +32,10 @@ var dayArr = [
     "FRIDAY",
     "SATURDAY"
 ];
-
 var d = new Date();
 
     var day = d.getDay();
     day = dayArr[day];
-
 
     var date = d.getDate();
 
@@ -39,8 +43,45 @@ var d = new Date();
     month = monthArr[month];
 
     var year = d.getFullYear();
-
     one.innerHTML = day;
-    two.innerHTML = date+"th";
+    two.innerHTML = date;
+    // figure out how to include th,st,and rd;
     three.innerHTML = month;
     four.innerHTML = year;
+    
+var daily = document.getElementById("numbers");
+
+if (month == "FEBRUARY" && year % 4 !== 0) {
+    for (i = 1; i <= 28; i++) {
+        const bee = document.createElement("p");
+        bee.innerHTML = i;
+        daily.appendChild(bee);
+    }
+}
+else if (month == "FEBRUARY" && year % 4 == 0) {
+    for (i = 1; i <= 29; i++) {
+        const bee = document.createElement("p");
+        bee.innerHTML = i;
+        daily.appendChild(bee);
+    }
+
+}
+else if (month.includes == month30Arr) {
+    for (i = 1; i <= 30; i++) {
+        const bee = document.createElement("p");
+        bee.innerHTML = i;
+        daily.appendChild(bee);
+    }
+}
+else {
+    for (i = 1; i <= 31; i++){
+            const bee = document.createElement("p");
+            bee.innerHTML = i;
+            daily.appendChild(bee);
+        }
+
+}
+
+
+
+    
